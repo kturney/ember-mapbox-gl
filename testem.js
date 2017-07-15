ad08bd5f84820c209fbf8b5c4fad6ce9e29bd63e
Kyle Turney <turney.kyle@gmail.com>
2017-07-15 14:24:32 -0500

    upgrade ember deps

--- testem.js
+++ testem.js
@@ -1,13 +1,13 @@
 /* eslint-env node */
 module.exports = {
-  "test_page": "tests/index.html?hidepassed",
-  "browser_start_timeout": 180,
-  "disable_watching": true,
-  "launch_in_ci": [
-    "Chrome",
-    "Firefox"
+  test_page: 'tests/index.html?hidepassed',
+  browser_start_timeout: 180,
+  disable_watching: true,
+  launch_in_ci: [
+    'Chrome',
+    'Firefox'
   ],
-  "launch_in_dev": [
-    "Chrome"
+  launch_in_dev: [
+    'Chrome'
   ]
 };
