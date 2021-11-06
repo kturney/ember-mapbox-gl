462ebaca374cfa3c0f7117c44f433969cea5b8ca
allthesignals <>
2021-11-06 01:51:29 -0500

    Test case popup rerender after close

--- tests/integration/components/mapbox-gl-popup-test.js
+++ tests/integration/components/mapbox-gl-popup-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
+import { render, click, settled } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 import setupMap from '../../helpers/create-map';
 
@@ -29,5 +29,23 @@ module('Integration | Component | mapbox gl popup', function (hooks) {
     this.map.fire('click');
 
     assert.verifySteps(['onClose']);
+  });
+
+  test('it handles re-renders on map clicks after closing', async function (assert) {
+    assert.expect(1);
+
+    this.set('clicked', { lngLat: { lng: -93.9688, lat: 37.1314 } });
+
+    await render(hbs`
+      {{#mapbox-gl-popup lngLat=(array this.clicked.lngLat.lng this.clicked.lngLat.lat) map=map MapboxGl=MapboxGl}}
+        Hi
+      {{/mapbox-gl-popup}}
+    `);
+
+    await click('.mapboxgl-popup-close-button');
+
+    this.set('clicked', { lngLat: { lng: -30.9688, lat: 36.1314 } });
+
+    assert.dom('.mapboxgl-popup-content').containsText('Hi');
   });
 });
