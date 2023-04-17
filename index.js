d85319e43e900974f4149d27e09ac46fdcccb909
johanrd <johanroed@gmail.com>
2023-04-17 19:03:30 +0200

    patch out `included` method to make sure equire.resolve is not starting from the wrong location (ember-cli instead of ember-mapbox-gl) when running with modern package managers

--- index.js
+++ index.js
@@ -13,18 +13,4 @@ module.exports = {
       plugins: [require.resolve('ember-auto-import/babel-plugin')],
     },
   },
-
-  included(app) {
-    this._super.included.apply(this, arguments);
-
-    const path = require('path');
-    const mapboxPkg = require(require.resolve('mapbox-gl/package.json', {
-      paths: [app.project.root],
-    }));
-    const stylesPath = require.resolve(`mapbox-gl/${mapboxPkg.style}`, {
-      paths: [app.project.root],
-    });
-
-    app.import(path.relative(app.project.root, stylesPath));
-  },
 };
